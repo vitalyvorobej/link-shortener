7e4a4f3f6f6581fd170a6e77f5e61f89bccf6f0c
v.varabei <vitalyvorobej@gmail.com>
2024-10-03 18:21:19 +0300

    Innitital commit

---
+++ src/main/java/com/home/linkshortener/LinkShortenerApp.java
@@ -0,0 +1,11 @@
+package com.home.linkshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkShortenerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(LinkShortenerApp.class, args);
+    }
+}
