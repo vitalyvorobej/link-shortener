37be12a2823fed6632382dead85136a65bfbf080
v.varabei <vitalyvorobej@gmail.com>
2024-11-28 22:21:52 +0300

    Task_11:[F1] Home Work pt11 -Added docker-compose.yaml -Added Dockerfile

--- src/main/java/com/course/project/varabei/linkshortener/configuration/LinkShortenerConfig.java
+++ src/main/java/com/course/project/varabei/linkshortener/configuration/LinkShortenerConfig.java
@@ -2,18 +2,20 @@ package com.course.project.varabei.linkshortener.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.ResourceUtils;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.FileCopyUtils;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
+import java.io.InputStream;
 
 @Configuration
 public class LinkShortenerConfig {
 
     @Bean
     public String notFoundPage() throws IOException {
-        File file = ResourceUtils.getFile("classpath:templates/404.html");
-        return Files.readString(file.toPath());
+        try (InputStream is = new ClassPathResource("templates/404.html").getInputStream()) {
+            byte[] binaryData = FileCopyUtils.copyToByteArray(is);
+            return new String(binaryData);
+        }
     }
 }
